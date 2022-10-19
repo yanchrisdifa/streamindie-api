e30ca219bc32df5d7083905c4d95f4dd6a4ab245
zetta-YanChris-Difa <chris.difa@zettabyte.life>
2022-10-20 07:42:41 +0800

    delete comment

--- keystone.ts
+++ keystone.ts
@@ -31,7 +31,6 @@ const {
 
 export default withAuth(
   config({
-    // the db sets the database provider - we're using sqlite for the fastest startup experience
     server: {
       cors: { origin: "*" },
     },
